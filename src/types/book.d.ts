e312f43f44a5d0a6d88d4fe2bcafdf1f0ce60114
chaiyut-kun <chwork28@hotmail.com>
2025-09-08 14:51:16 +0700

    initialize projects

---
+++ src/types/book.d.ts
@@ -0,0 +1,34 @@
+interface User {
+  _id: string;
+  username: string;
+  email: string;
+}
+
+interface Book {
+    _id: string;
+    title: string;
+    author: string
+    description: string
+    genre: string
+    year: number
+    price: number
+    available: boolean;
+    addedBy: User;
+    createdAt: string; // ISO date string
+    updatedAt: string; // ISO date string
+}
+
+interface Pagination {
+  page: number;
+  limit: number;
+  total: number;
+  pages: number;
+}
+
+// Root response model
+interface BookResponse {
+  books: Book[];
+  pagination: Pagination;
+}
+
+export type { User, Book, Pagination, BookResponse };
