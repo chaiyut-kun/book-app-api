35b63d52689e3f559eeb3ee97dc6521e7a2c9f1e
chaiyut-kun <chwork28@hotmail.com>
2025-09-10 00:32:38 +0700

    add book card components

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Book, BookResponse } from "@/types/book";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Card, CardActions, CardContent, Button, Typography } from '@mui/material';
 
 
 export default function Page() {
@@ -26,8 +27,52 @@ export default function Page() {
     return (
         <div>
             <h1>Book ID: {id}</h1>
-            <h2>Book Author: {book?.author}</h2>
+
+            <BasicCard author={book?.author} title={book?.title} description={book?.description} price={book?.price}></BasicCard>
+            
         </div>
     )
     
+}
+
+
+
+export function BasicCard({ author, title, description, price, year, available }: BookCardProps) {
+  return (
+    <>
+    <Card sx={{ width: 500, minWidth: 275 }}>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+          {author}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+        <Typography variant="body2">
+          {description}
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">
+            {price} ฿
+        </Button>
+      </CardActions>
+    </Card>
+
+    <Card variant="outlined" sx={{ width: 500, minWidth: 275 }} className="mt-4">
+        <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                ตีพิมพ์ในปี : {year}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                สถานะ : {available}
+            </Typography>
+              
+        </CardContent>
+    </Card>
+    </>
+  );
 }
---
+++ src/types/card.d.ts
@@ -0,0 +1,9 @@
+interface BookCardProps {
+    title: string;
+    author: string;
+    description: string;
+    price: number;
+    year?: number;
+    available?: boolean;
+    
+}
