c511681607f4753f61d30d2a174075565eac7654
chaiyut-kun <chwork28@hotmail.com>
2025-09-15 16:25:31 +0700

    add authentication service

--- src/lib/AuthService.ts
+++ src/lib/AuthService.ts
@@ -1,9 +1,19 @@
+import { RegisterForm } from "@/types/RegisterForm"
 import axios from "axios"
 
 export default class AuthService {
-    static Register(data: any) {
+    static Register(data: RegisterForm) {
         // send data to api
         const response = axios.post('http://localhost:3000/api/auth/register', data)
-        
+        return response
     }
+}
+
+export function ValidatePassword(password: string, confirmPassword: string) {
+    return password === confirmPassword && password.length >= 6
+}
+
+export function ValidateEmail(email: string) {
+    const re = /\S+@\S+\.\S+/
+    return re.test(email)
 }
